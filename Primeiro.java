c23d9c9673afd1f5daf4a9b64aa2cf9fc212abe7
Elberth Moraes <elberth.moraes@prof.infnet.edu.br>
2021-10-08 09:21:16 -0300

    Atualizacao atraves de metodos

--- Primeiro.java
+++ Primeiro.java
@@ -1,8 +1,37 @@
 public class Primeiro {
+	
+	private static boolean validar(int qtde){		
+		if(qtde == 6){
+			return true;
+		}	
+		return false;
+	}
+	
+	private static int definirAnoNascimento(int aIdade){
+		return 2021 - aIdade;
+	}
+	
+	private static float calcularValorTotal(float salarioMensal, int meses){
+		return salarioMensal * meses;
+	}
 
+	private static String definirStatus(int aIdade){
+		if(aIdade <= 50){
+			return "iniciante";
+		}		
+		return "veterano";
+	}
+	
+	private static String definirSituacao(float oSalario){
+		if(oSalario > 1000){
+			return "estavel";
+		}		
+		return "instavel";			
+	}
+	
 	public static void main(String[] args){
 				
-		if(args.length == 6){
+		if(validar(args.length)){
 
 			String nome = args[0];
 			String sobrenome = args[1];
@@ -11,8 +40,13 @@ public class Primeiro {
 			boolean ehJava = Boolean.valueOf(args[4]);
 			int qtdeMeses = Integer.valueOf(args[5]);
 
-			int anoNascimento = 2021 - idade;
-			float valorTotal = salario * qtdeMeses;
+			int anoNascimento = definirAnoNascimento(idade);
+			
+			float valorTotal = calcularValorTotal(salario, qtdeMeses);
+			
+			String status = definirStatus(idade);
+			
+			String situacao = definirSituacao(salario);
 
 			System.out.println("Nome: " + nome);
 			System.out.println("Sobrenome: " + sobrenome);
@@ -21,6 +55,8 @@ public class Primeiro {
 			System.out.println("Salario: " + salario);
 			System.out.println("Valor total: " + valorTotal);
 			System.out.println("Java dev: " + ehJava);		
+			System.out.println("Status: " + status);
+			System.out.println("Situacao: " + situacao);
 		} else {
 			System.out.println("A quantidade " + args.length + " de argumentos esta incorreta!!!");
 		}
